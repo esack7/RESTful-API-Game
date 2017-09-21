50ae89ed69af190689e4aaca871ee6eea1d319ac
saidmattar <ssabdou@gmail.com>
2017-09-21 13:29:46 -0700

    whaaaat

--- __test__/routes/game.test.js
+++ __test__/routes/game.test.js
@@ -11,25 +11,56 @@ describe('testing game routes', function() {
   beforeAll(server.start);
   afterAll(server.stop);
   afterEach(mocks.user.removeAll);
+  describe('POST to api/game', () => {
+    describe('valid request', () => {
+      beforeAll( () => {
+        return mocks.user.createOne()
+          .then(userData => {
+            return superagent.post(':4444/api/game')
+              .type('application/json')
+              .set('Authorization', `Bearer ${userData.token}`)
+              .send('{"mapName": "map1"}')
+              .then(res => this.res = res);
+          });
+      });
+      // console.log(mocks.user);
+      test('should return a status of 200', () => {
+        expect(this.res.status).toBe(200);
+      });
+      test('text response should contain the file name', () => {
+        let txt = this.res.text.split('.');
+        let file = JSON.parse(this.res.request._data);
+        expect(txt[0]).toBe(`New game started using ${file.mapName} file`);
+      });
+    });
+    describe('invalid POST to api/game', () => {
+      test('should return a status of 404', () => {
+        return mocks.user.createOne()
+          .then(userData => {
+            return superagent.post(':4444/api/game')
+              .type('application/json')
+              .set('Authorization', `Bearer ${userData.token}`)
+              .send('{"mapName": "doesNotExist"}')
+              .catch(err => {
+                expect(err.status).toBe(404);
+              });
+          });
+      });
+      test('should return a status of 404', () => {
+        return mocks.user.createOne()
+          .then(userData => {
+            return superagent.post(':4444/api/game')
+              .type('application/json')
+              .set('Authorization', `Bearer ${userData.token}`)
+              .send('{"mapName": "doesNotExist"}')
+              .catch(err => {
+                // console.log(err.response.text);
+                expect(err.response.text).toBe('Error: This file does not exist');
+              });
+          });
+      });
+    });
 
-  describe('POST to api/game', function() {
-    beforeAll( () => {
-      return mocks.user.createOne()
-        .then(userData => {
-          return superagent.post(':4444/api/game')
-            .type('application/json')
-            .set('Authorization', `Bearer ${userData.token}`)
-            .send('{"mapName": "map1"}')
-            .then(res => this.res = res);
-        });
-    });
-    test('should return a status of 200', () => {
-      expect(this.res.status).toBe(200);
-    });
-    test('text response should contain the file name', () => {
-      let txt = this.res.text.split('.');
-      let file = JSON.parse(this.res.request._data);
-      expect(txt[0]).toBe(`New game started using ${file.mapName} file`);
-    });
   });
+
 });
